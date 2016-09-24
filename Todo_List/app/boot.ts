0f7b9e3c220760045dc84726837b438caf6d57f0
anashussain08 <anas_hussain08@yahoo.com>
2016-09-24 19:23:27 +0500

    updated to final release

--- Todo_List/app/boot.ts
+++
@@ -1,5 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {MainComponent} from './app';
-import {MATERIAL_PROVIDERS} from 'ng2-material/all';
-    
-bootstrap(MainComponent,[MATERIAL_PROVIDERS]);
